ad7a77fa99f7b3c32df7b9bceceffe75972ef267
seigiakatsuka <minuitspence@gmail.com>
2025-08-26 12:43:12 -0400

    updating dashboard project after completing chapter 11

---
+++ app/dashboard/(overview)/loading.tsx
@@ -0,0 +1,6 @@
+import DashboardLayout from "@/app/dashboard/layout";
+import DashboardSkeleton from "@/app/ui/skeletons";
+
+export default function Loading() {
+    return <DashboardSkeleton />
+}
